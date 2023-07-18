e0a286efe035589e548b463c2ed111d6c48869f5
Zhong Wu <zhong.wu@autodesk.com>
2023-07-18 08:48:37 +0800

    improve plugin code

--- ExportImportExcelPlugin/ExportMEPSystemGraphs.cs
+++ ExportImportExcelPlugin/ExportMEPSystemGraphs.cs
@@ -35,7 +35,6 @@ using Autodesk.Revit.DB;
 
 using DesignAutomationFramework;
 
-using libxl;
 using Autodesk.Revit.DB.Mechanical;
 using Autodesk.Revit.DB.Plumbing;
 using Autodesk.Revit.DB.Electrical;
@@ -51,11 +50,9 @@ namespace ExportMEPSystemGraphs
     [Autodesk.Revit.Attributes.Transaction(Autodesk.Revit.Attributes.TransactionMode.Manual)]
     public class MEPSystemHandler : IExternalDBApplication
     {
-        public const string FireRating = "Fire Rating";
-        public const string Comments = "Comments";
+        //public const string FireRating = "Fire Rating";
+        //public const string Comments = "Comments";
         public const string NodeLabelTag = "text";
-
-
 
 
         /// <summary>
@@ -94,19 +91,6 @@ namespace ExportMEPSystemGraphs
                   : MepDomain.Invalid));
         }
 
-        /// <summary>
-        /// Create a and return the path of a random temporary directory.
-        /// </summary>
-        static string GetTemporaryDirectory()
-        {
-            string tempDirectory = Path.Combine(
-              Path.GetTempPath(), Path.GetRandomFileName());
-
-            Directory.CreateDirectory(tempDirectory);
-
-            return tempDirectory;
-        }
-
 
         public ExternalDBApplicationResult OnStartup(ControlledApplication app)
         {
@@ -123,6 +107,7 @@ namespace ExportMEPSystemGraphs
         {
             e.Succeeded = ProcessParameters(e.DesignAutomationData);
         }
+
 
         public static bool ProcessParameters(DesignAutomationData data)
         {
@@ -143,11 +128,11 @@ namespace ExportMEPSystemGraphs
 
             InputParams inputParams = InputParams.Parse("params.json");
 
-            return FindConnectedElements( rvtApp, doc, inputParams);
+            return FindConnectedElements(doc, inputParams);
         }
 
 
-        public static bool FindConnectedElements( Application rvtApp, Document doc, InputParams parameters )
+        public static bool FindConnectedElements(Document doc, InputParams parameters)
         {
             FilteredElementCollector allSystems
               = new FilteredElementCollector(doc)
@@ -225,7 +210,9 @@ namespace ExportMEPSystemGraphs
 
                 foreach (MEPSystem system in desirableSystems)
                 {
-                    Debug.Print(system.Name);
+                    //Debug.Print(system.Name);
+                    Console.WriteLine(system.Name);
+
 
                     FamilyInstance root = system.BaseEquipment;
 
@@ -252,10 +239,10 @@ namespace ExportMEPSystemGraphs
                           ? tree.DumpToJsonBottomUp()
                           : tree.DumpToJsonTopDown();
 
-                        Debug.Assert(2 < json.Length,
-                          "expected valid non-empty JSON graph data");
+                        //Debug.Assert(2 < json.Length,"expected valid non-empty JSON graph data");
+                        //Debug.Print(json);
 
-                        Debug.Print(json);
+                        Console.WriteLine(json);
 
                         // Save this system hierarchy JSON in the
                         // appropriate domain specific collector.
@@ -356,7 +343,9 @@ namespace ExportMEPSystemGraphs
               doc.Title.GetHashCode().ToString(),
               doc.Title, json_systems);
 
-            Debug.Print(json);
+            //Debug.Print(json);
+            Console.WriteLine(json);
+
 
             if (parameters.StoreEntireJsonGraphOnProjectInfo)
             {
@@ -377,272 +366,6 @@ namespace ExportMEPSystemGraphs
             return true;
 
         }
-
-
-        //public static bool ImportFromExcel(Application rvtApp, Document doc, bool includeFireRating, bool includeComments)
-        //{
-        //    Book book = new BinBook();
-        //    book.load("input.xls");
-
-        //    if (includeFireRating)
-        //    {
-        //        // 1st sheet is FireRating
-        //        Sheet worksheet = book.getSheet(0);
-        //        if (worksheet == null)
-        //        {
-        //            throw new FileLoadException("Could not get worksheet.");
-        //        }
-        //        using (Transaction t = new Transaction(doc))
-        //        {
-        //            t.Start("Import FireRating parameters from Excel");
-        //            // Starting from row 2, loop the rows and extract params.
-        //            int id;
-        //            string fireRatingValue;
-        //            int row = 2;
-
-        //            int count = worksheet.lastRow();
-        //            Console.WriteLine("Total Row is: " + count);
-        //            while (row < count)
-        //            {
-        //                try
-        //                {
-        //                    // Extract relevant XLS values.
-        //                    Console.WriteLine("Read the Row: " + row.ToString());
-
-        //                    id = (int)worksheet.readNum(row, 1);
-        //                    if (0 >= id)
-        //                    {
-        //                        continue;
-        //                    }
-        //                    Console.WriteLine("Read the elemnt Id: " + id.ToString());
-
-        //                    // Read FireRating value
-        //                    fireRatingValue = worksheet.readStr(row, 5);
-        //                    Console.WriteLine("Read the fire rating value: " + fireRatingValue);
-
-        //                    // Get document's door element via Id
-        //                    ElementId elementId = new ElementId(id);
-        //                    Element doorType = doc.GetElement(elementId);
-
-        //                    // Set the param
-        //                    if (null != doorType)
-        //                    {
-        //                        Console.WriteLine("Read the fire rating value of the door: " + doorType.Id.ToString());
-        //                        Parameter fireRatingParam =  doorType.get_Parameter(BuiltInParameter.DOOR_FIRE_RATING);
-        //                        if( null != fireRatingParam)
-        //                        {
-        //                            if (!fireRatingParam.IsReadOnly)
-        //                            {
-        //                                fireRatingParam.Set(fireRatingValue);
-        //                                Console.WriteLine("write row: " + row.ToString() + " id: " + doorType.Id.IntegerValue.ToString() + " value: " + fireRatingParam.AsString());
-        //                            }
-        //                        }
-        //                    }
-        //                    Console.WriteLine("Set the parameters of " + elementId.ToString());
-        //                }
-        //                catch (System.Exception e)
-        //                {
-        //                    Console.WriteLine("Get the exception of " + e.Message);
-        //                    break;
-        //                }
-        //                ++row;
-        //            }
-        //            t.Commit();
-        //            Console.WriteLine("Commit the FireRating opearation.");
-        //        }
-
-        //    }
-        //    if (includeComments)
-        //    {
-        //        // 2nd sheet is Comments
-        //        Sheet worksheet = book.getSheet(1);
-        //        if (worksheet == null)
-        //        {
-        //            throw new FileLoadException("Could not get worksheet.");
-        //        }
-        //        using (Transaction t = new Transaction(doc))
-        //        {
-        //            t.Start("Import Comments Values from Excel");
-        //            // Starting from row 2, loop the rows and extract Id and FireRating param.
-        //            int id;
-        //            string comments;
-        //            int row = 2;
-
-        //            int count = worksheet.lastRow();
-        //            Console.WriteLine("Total Row is: " + count);
-        //            while (row < count)
-        //            {
-        //                try
-        //                {
-        //                    // Extract relevant XLS values.
-        //                    Console.WriteLine("Read the Row: " + row.ToString());
-        //                    id = (int)worksheet.readNum(row, 1);
-        //                    if (0 >= id)
-        //                    {
-        //                        continue;
-        //                    }
-        //                    Console.WriteLine("Read the elemnt Id: " + id.ToString());
-
-        //                    // Read Door instance comments value
-        //                    comments = worksheet.readStr(row, 4);
-        //                    Console.WriteLine("Read the comments value: " + comments);
-
-        //                    ElementId elementId = new ElementId(id);
-        //                    Element door = doc.GetElement(elementId);
-
-        //                    // Set the param
-        //                    if (null != door)
-        //                    {
-        //                        Console.WriteLine("Read the comments value of the door: " + door.Id.ToString());
-        //                        Parameter commentsParam = door.get_Parameter(BuiltInParameter.ALL_MODEL_INSTANCE_COMMENTS);
-        //                        if (null != commentsParam)
-        //                        {
-        //                            if (!commentsParam.IsReadOnly)
-        //                            {
-        //                                commentsParam.Set(comments);
-        //                                Console.WriteLine("write row: " + row.ToString() + " id: " + door.Id.IntegerValue.ToString() + " value: " + commentsParam.AsString());
-        //                            }
-        //                        }
-        //                    }
-        //                    Console.WriteLine("Set the parameters of " + elementId.ToString());
-        //                }
-        //                catch (System.Exception e)
-        //                {
-        //                    Console.WriteLine("Get the exception of " + e.Message);
-        //                    break;
-        //                }
-        //                ++row;
-        //            }
-        //            t.Commit();
-        //            Console.WriteLine("Commit the Comments opearation.");
-        //        }
-        //    }
-
-        //    ModelPath path = ModelPathUtils.ConvertUserVisiblePathToModelPath("result.rvt");
-        //    doc.SaveAs(path, new SaveAsOptions());
-        //    Console.WriteLine("Revit File is saved at "+ path.CentralServerPath);
-        //    return true;
-        //}
-
-        //public static bool ExportToExcel( Application rvtApp, Document doc, bool includeFireRating, bool includeComments )
-        //{
-        //    Console.WriteLine("start create Excel.");
-        //    Book book = new BinBook(); // use XmlBook() for xlsx
-        //    if (null == book)
-        //    {
-        //        throw new InvalidOperationException("Could not create BinBook.");
-        //    }
-
-        //    if (includeFireRating)
-        //    {
-        //        Sheet sheet = book.addSheet("Door Type FireRating");
-        //        sheet.writeStr(1, 1, "Element ID");
-        //        sheet.writeStr(1, 2, "Unique ID");
-        //        sheet.writeStr(1, 3, "Family Name");
-        //        sheet.writeStr(1, 4, "Family Type");
-        //        sheet.writeStr(1, 5, FireRating);
-
-        //        List<Element> elems = GetTargetElements(doc, BuiltInCategory.OST_Doors, false);
-        //        // Loop through all elements and export each to an Excel row
-
-        //        int row = 2;
-        //        foreach (Element e in elems)
-        //        {
-        //            sheet.writeNum(row, 1, e.Id.IntegerValue);
-        //            sheet.writeStr(row, 2, e.UniqueId);
-
-        //            FamilySymbol symbol = e as FamilySymbol;
-        //            if( null != symbol)
-        //            {
-        //                sheet.writeStr(row, 3, symbol.FamilyName);
-        //                sheet.writeStr(row, 4, symbol.Name);
-
-        //                // FireRating:
-        //                Parameter fireRatingParam = symbol.get_Parameter(BuiltInParameter.DOOR_FIRE_RATING);
-        //                if (fireRatingParam != null)
-        //                {
-        //                    sheet.writeStr(row, 5, fireRatingParam.AsString());
-        //                    Console.WriteLine("write row " + row.ToString() + ":" + e.Id.IntegerValue.ToString() + "," + e.UniqueId + "," + symbol.FamilyName + "," + symbol.Name + "," + fireRatingParam.AsString());
-        //                }
-        //            }
-        //            ++row;
-        //        }
-        //    }
-
-        //    if ( includeComments)
-        //    {
-        //        Sheet sheet = book.addSheet("Door Instance Comments");
-        //        sheet.writeStr(1, 1, "Element ID");
-        //        sheet.writeStr(1, 2, "Unique ID");
-        //        sheet.writeStr(1, 3, "Instance Name");
-        //        sheet.writeStr(1, 4, Comments);
-
-        //        Console.WriteLine("write comments");
-
-        //        List<Element> elems = GetTargetElements(doc, BuiltInCategory.OST_Doors, true);
-        //        // Loop through all elements and export each to an Excel row
-        //        int row = 2;
-        //        foreach (Element e in elems)
-        //        {
-        //            sheet.writeNum(row, 1, e.Id.IntegerValue);
-        //            sheet.writeStr(row, 2, e.UniqueId);
-        //            sheet.writeStr(row, 3, e.Name);
-
-        //            // Comments:
-        //            Parameter commentsParam = e.get_Parameter(
-        //              BuiltInParameter.ALL_MODEL_INSTANCE_COMMENTS);
-        //            if (null != commentsParam)
-        //            {
-        //                sheet.writeStr(row, 4, commentsParam.AsString());
-        //                Console.WriteLine("write row " + row.ToString() + ":" + e.Id.IntegerValue.ToString() + "," + e.UniqueId + ","+e.Name + "," + commentsParam.AsString());
-        //            }
-        //            ++row;
-        //        }
-        //    }
-        //    book.save("result.xls");
-        //    Console.WriteLine("Excel App is created");
-        //    return true;
-        //}
-
-        ///// <summary>
-        ///// Helper to get all Elements for a given category,
-        ///// identified either by a built-in category or by a category name.
-        ///// </summary>
-        //public static List<Element> GetTargetElements(
-        //  Document doc,
-        //  object targetCategory,
-        //  bool isInstance)
-        //{
-        //    List<Element> elements;
-
-        //    bool isName = targetCategory.GetType().Equals(typeof(string));
-
-        //    if (isName)
-        //    {
-        //        Category cat = doc.Settings.Categories.get_Item(targetCategory as string);
-        //        FilteredElementCollector collector = new FilteredElementCollector(doc);
-        //        collector.OfCategoryId(cat.Id);
-        //        elements = new List<Element>(collector);
-        //    }
-        //    else
-        //    {
-        //        FilteredElementCollector collector = null;
-        //        if (isInstance)
-        //        {
-        //            collector = new FilteredElementCollector(doc).WhereElementIsNotElementType();
-
-        //        }
-        //        else
-        //        {
-        //            collector = new FilteredElementCollector(doc).WhereElementIsElementType();
-
-        //        }
-
-        //        collector.OfCategory((BuiltInCategory)targetCategory);
-        //        elements = collector.ToList<Element>();
-        //    }
-        //    return elements;
-        //}
     }
 
 
